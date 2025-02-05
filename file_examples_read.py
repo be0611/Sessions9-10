7cd0436f96f6c361e2a4dc70b1cd3c05299e6fb7
be0611 <bernardo.g.araujo@icloud.com>
2025-02-05 17:03:35 +0100

    read examples

---
+++ file_examples_read.py
@@ -0,0 +1,17 @@
+fp = open("text.txt","r") # r i by default, so not really needed
+print(fp.read()) # prints the entire content of the file
+fp.close() # good practice to close the file
+
+# same exact thing with context manager
+with open ("text.txt", "r") as fp:
+    print(fp.read())
+
+# lets read from the same file, line by line
+print("Read the file line by line")
+line_number = 1
+with open("text.txt", "r") as fp:
+    for line in fp: # we iterate over the file, line by line
+        print(f"{line_number}: {line}", end="")
+        # print(line.rstrip())
+        line_number += 1
+print("done printing")
